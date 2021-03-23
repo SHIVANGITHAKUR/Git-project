d38e122d177a71c9264c1771cbf26fbacd108595
Shivangi <41469660+SHIVANGITHAKUR@users.noreply.github.com>
2021-03-23 10:58:51 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
-    printf("Welcome to Github Actions.\n********* Its Working*******\n");
+    printf("Welcome to Github Actions.\n);
+    printf("\n*********Hurry, It Works*******\n");
     return 0;
 }
